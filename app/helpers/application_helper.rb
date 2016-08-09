6bd0f56b8dfb2b4ceea825be7fdee6ce92bb7324
Aoife McDaid <aoife.mcdaid@gmail.com>
2016-08-09 20:49:03 +0200

    rails wikinimous

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+ def markdown(text)
+   return "" if text.blank?
+   markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+   markdown.render(text).html_safe
+ end
 end
